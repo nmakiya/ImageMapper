a4e757dd5aedd66af98837773158bd8d17f314d7
Naseem Makiya <naseem.makiya@moovweb.com>
2015-10-04 12:43:17 -0700

    adding some error catching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from Exception, :with => :not_found
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+  def not_found
+    flash[:notice] = "Oops! Page does not exist."
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/images/new' => "images#create"
   post '/images/:id/update' => "images#update"
   get '/images/:id' => "images#show"
-
   root to: "home#index"
+
+  get '*unmatched_route', to: 'application#not_found'
 end
